d94f36afaf4ab89d89bc2f63e2af28baac0bfadd
antariksha-zomind <antariksha.sharma@zomind.ai>
2026-01-14 05:03:00 +0530

    Create model for book

---
+++ src/main/java/com/example/model/Book.java
@@ -0,0 +1,18 @@
+package com.example.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+  private Integer id;
+  private String name;
+  private String author;
+  private Double price;
+}
